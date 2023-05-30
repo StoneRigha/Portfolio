25f5581b28d8a6a8776d14207185134470b72c09
Livingstone Maghanga <stoneriga@gmail.com>
2023-05-30 17:14:12 +0300

    image update

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,7 +73,7 @@ export default function Home() {
                   </a>
                 
               </div>
-              <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt=20 overflow-hidden md:h-96 md:w-96'>
+              <div className='relative mx-auto mr-2 bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt=20 overflow-hidden md:h-96 md:w-96'>
                 <Image className='' src={ras} alt='image' layout='fill' objectFit='cover'  />
               </div>
             </section>
